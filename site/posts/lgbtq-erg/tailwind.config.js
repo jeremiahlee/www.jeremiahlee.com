6ff65d07a031dd898cb71b1fb82bdd83551305cd
Jeremiah Lee <mass@jeremiahlee.com>
2021-09-23 18:29:21 +0200

    Add LGBTQ+ ERG post

---
+++ site/posts/lgbtq-erg/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	mode: "jit",
+	purge: [
+		"./site/posts/lgbtq-erg/lgbtq-erg.html",
+		"./eleventy-includes/lgbtq-erg.hbs"
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [
+	],
+}
