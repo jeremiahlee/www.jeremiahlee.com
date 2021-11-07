ebd59d37019a990116d6ccd72d8ec92e326d76fb
Jeremiah Lee <mass@jeremiahlee.com>
2021-11-07 14:31:13 +0100

    Add Thanksgiving rsvp redirect

--- generate-redirects.js
+++ generate-redirects.js
@@ -26,7 +26,8 @@ const redirects = [
 	["/blog/2007/05/13/evvy-26/", "/posts/evvy-26/"],
 	["/blog/2005/07/30/explicitly-me/", "/posts/explicitly-me/"],
 	["/blog/2003/05/10/mom-monologue/", "/posts/mom-monologue/"],
-	["/rsvp/", "https://jeremiahlee.typeform.com/to/tsZCL1"],
+	["/rsvp", "https://jeremiahlee.typeform.com/to/xLt07lui"],
+	["/rsvp/", "https://jeremiahlee.typeform.com/to/xLt07lui"],
 	["/posts/failed-squad-goals", "/posts/failed-squad-goals/"]
 ];
 
