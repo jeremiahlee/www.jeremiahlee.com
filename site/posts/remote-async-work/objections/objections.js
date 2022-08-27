f969ed20001d057af4661162beb863150ac9bedc
Jeremiah Lee <106775223+jeremiahlee@users.noreply.github.com>
2022-08-27 17:07:37 +0200

    Make exclusive RAW articles less exclusive

--- site/posts/remote-async-work/objections/objections.js
+++ site/posts/remote-async-work/objections/objections.js
@@ -4,6 +4,7 @@ window.addEventListener(
 		function showContent() {
 			document.getElementById("exclusive").classList.remove("hidden");
 			document.getElementById("pitch").classList.add("hidden");
+			window.scroll(0,0);
 		}
 
 		if (document.monetization) {
@@ -11,12 +12,11 @@ window.addEventListener(
 				"monetizationstart",
 				showContent
 			);
-		} else {
-			const urlParameters = new URLSearchParams(document.location.search.substring(1));
-			const accessCode = urlParameters.get("accessCode");
-			if (accessCode === "ViaReal") {
-				showContent();
-			}
 		}
+
+		document.getElementById("skip-button").addEventListener(
+			"click",
+			showContent
+		);
 	}
 );
