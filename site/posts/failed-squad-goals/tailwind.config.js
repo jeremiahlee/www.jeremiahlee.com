a088c1f3eee912b59d6df2c5a4c07a7af259a374
Jeremiah Lee <106775223+jeremiahlee@users.noreply.github.com>
2022-08-29 17:30:29 +0200

    Improve Failed Squad Goals post-read conversion
    - Add email list sign-up
    - Add related articles
    - Fix minimum vertical padding on spread
    - Add link to cover explanation

--- site/posts/failed-squad-goals/tailwind.config.js
+++ site/posts/failed-squad-goals/tailwind.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	content: [
 		"./site/posts/failed-squad-goals/failed-squad-goals.html",
 		"./eleventy-includes/failed-squad-goals.hbs"
+	],
+	plugins: [
+		require("@tailwindcss/forms")
 	]
 }
